54bb58e8373faca3efabe01219d4a47acca21ab0
Yifan Cheng <yifacheng@student.unimelb.edu.au>
2023-09-21 17:04:48 +1000

    Revert "Revert "Merge branch 'feature/child-acct-creation/luchen' into feature/phone-call/yifan""
    This reverts commit 8f49c2c61bf961579c6aa940f7e66b3fd918fd4f.

--- app/src/main/java/com/example/safecircle/NavigationComposable.kt
+++ app/src/main/java/com/example/safecircle/NavigationComposable.kt
@@ -1,16 +1,38 @@
 package com.example.safecircle
 
+import com.example.safecircle.utils.PreferenceHelper
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.safecircle.ui.screen.AccountSettingsScreen
+import com.example.safecircle.ui.screen.ConnectionSettingsScreen
+import com.example.safecircle.ui.screen.DashboardScreen
+import com.example.safecircle.ui.screen.LandingScreen
+import com.example.safecircle.ui.screen.LoginScreen
+import com.example.safecircle.ui.screen.RegisterScreen
+import com.example.safecircle.viewmodel.RegisterViewModel
 
 @Composable
 fun NavigationComposable(navController: NavHostController){
-    val startRoute = Register.route
+    val context = LocalContext.current
+    val preferenceHelper = PreferenceHelper(context)
+    val familyId = preferenceHelper.getFamilyID()
+    val username = preferenceHelper.getUsername()
+
+    val startRoute = if (!familyId.isNullOrEmpty() && !username.isNullOrEmpty()) {
+        Dashboard.route
+    } else {
+        Register.route
+    }
+
+    val registerViewModel: RegisterViewModel = remember { RegisterViewModel(context) }
+
     NavHost(navController = navController, startDestination = startRoute){
         composable(Register.route){
-            RegisterScreen(navController)
+            RegisterScreen(navController, registerViewModel)
         }
         composable(Login.route){
             LoginScreen(navController)
@@ -21,8 +43,11 @@ fun NavigationComposable(navController: NavHostController){
         composable(Dashboard.route){
             DashboardScreen(navController)
         }
-        composable(MapChild.route){
-            MapChildScreen(navController)
+        composable(AccountSettings.route){
+            AccountSettingsScreen(navController)
+        }
+        composable(ConnectionSettings.route){
+            ConnectionSettingsScreen(navController)
         }
     }
 }
