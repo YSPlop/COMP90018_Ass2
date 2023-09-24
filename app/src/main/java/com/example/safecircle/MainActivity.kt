9f7417f7938102f2b8b5bdea8913415aa14e3809
Luchen2001 <luczhou@student.unimelb.edu.au>
2023-09-24 18:26:23 +1000

    bugfix: import error fixed

--- app/src/main/java/com/example/safecircle/MainActivity.kt
+++ app/src/main/java/com/example/safecircle/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.safecircle
 import android.content.Intent
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
