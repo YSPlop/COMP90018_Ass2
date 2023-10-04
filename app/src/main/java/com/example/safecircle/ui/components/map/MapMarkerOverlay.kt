d0cd7ebfb70139e2cfb5b85b89a932f35b596068
Alex Zhang <zhangchi0104@live.com>
2023-10-04 11:18:37 +1100

    feat: Updated marker style

--- app/src/main/java/com/example/safecircle/ui/components/map/MapMarkerOverlay.kt
+++ app/src/main/java/com/example/safecircle/ui/components/map/MapMarkerOverlay.kt
@@ -2,21 +2,12 @@ package com.example.safecircle.ui.components.map
 
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-
-import androidx.compose.foundation.layout.size
-
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.safecircle.ui.components.TextIcon
 import com.example.safecircle.viewmodel.MapViewModel
 import com.google.android.gms.maps.CameraUpdate
@@ -44,12 +35,15 @@ fun MapMarkerOverlay(viewModel: MapViewModel) {
         }
     }
     viewModel.memberLocations.entries.map {
+        val name = it.key
+            .split(" ")
+            .joinToString(separator = "") { p -> p[0].uppercase() }
         MarkerComposable(
             state = MarkerState(it.value),
-            title =  it.key,
+            title = it.key,
 
-        ) {
-            TextIcon(text = "AA")
+            ) {
+            TextIcon(text = name)
         }
     }
 }
