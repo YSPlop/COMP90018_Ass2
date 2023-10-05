ae613f3f0b672099488b64e4df4bfb7417e6515c
Luchen2001 <110806242+Luchen2001@users.noreply.github.com>
2023-10-05 11:49:09 +1100

    feature: map-maker/alex (#8)
    * feat: Display markers on the map
    
    * feat: Updated marker style
    
    ---------
    
    Co-authored-by: Alex Zhang <zhangchi0104@live.com>

---
+++ app/src/main/java/com/example/safecircle/viewmodel/MapViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.safecircle.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.safecircle.database.FamilyLocationDao
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+
+
+class MapViewModel(
+    private val familyId: String
+): ViewModel() {
+    var memberLocations by mutableStateOf(mapOf<String, LatLng>())
+        private set
+    private var _familyLocationDao = FamilyLocationDao.getInstance(familyId)
+    val cameraState: CameraPositionState
+    init {
+        _familyLocationDao.listenForCurrentLocationChanges {
+            Log.d("LocationShringScreenViewModel", it.toString())
+            memberLocations = it
+
+        }
+
+        val uniMelbCoord = LatLng(-37.798919,144.964232)
+        val cameraPosition = CameraPosition.fromLatLngZoom(uniMelbCoord, 15f)
+        cameraState = CameraPositionState(cameraPosition)
+    }
+     fun fetchMemberLocationsAsync() {
+         _familyLocationDao.getMembersLocationsAsync { memberLocations = it }
+     }
+
+
+}
