0d91ef189c465022bba6a9232167a7159121f2d7
Yukash <sivarajyukash@gmail.com>
2023-10-10 12:32:07 +1100

    UI Refinement

--- app/src/main/java/com/example/safecircle/ui/screen/DashboardScreen.kt
+++ app/src/main/java/com/example/safecircle/ui/screen/DashboardScreen.kt
@@ -1,6 +1,7 @@
 package com.example.safecircle.ui.screen
 
 import android.app.ActivityManager
+import android.app.Person
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -34,13 +35,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import com.example.safecircle.Map
 import com.example.safecircle.database.FamilyDatabase
 import com.example.safecircle.sensors.ForegroundSensorService
 import com.example.safecircle.ui.components.AppDrawer
 import com.example.safecircle.ui.components.AppTopBar
+import com.example.safecircle.ui.theme.CyanSecondary
+import com.example.safecircle.ui.theme.YellowPrimary
 import com.example.safecircle.utils.PreferenceHelper
+
 
 
 data class PersonInfo(
@@ -120,7 +129,12 @@ fun DashboardScreen(navController: NavHostController) {
         drawerState = drawerState,
         drawerContent = { AppDrawer(drawerState, navController) }
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = CyanSecondary)
+
+        ) {
             AppTopBar(drawerState, "Dashboard")
             LazyColumn(
                 modifier = Modifier
@@ -128,9 +142,7 @@ fun DashboardScreen(navController: NavHostController) {
                     .padding(16.dp)
             ) {
                 items(childrenList) { person ->
-                    PersonCard(person = person, navController) {
-
-                    }
+                    PersonCard(person = person, navController)
                     Spacer(modifier = Modifier.height(8.dp))
                 }
             }
@@ -138,51 +150,156 @@ fun DashboardScreen(navController: NavHostController) {
     }
 }
 
+/*
+* I removed the functionality of passing in an onClick function
+* Can revert to main branch code
+* */
 @Composable
-fun PersonCard(person: PersonInfo, navController: NavHostController, onClick: () -> Unit) {
-    Surface(
+fun PersonCard(person: PersonInfo, navController: NavHostController) {
+    ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(vertical = 5.dp, horizontal = 10.dp)
             .background(
                 color = MaterialTheme.colorScheme.surface,
                 shape = RoundedCornerShape(8.dp)
             )
             .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
-            .clickable(onClick = {
-                navController.navigate(Map.ofUser(person.name))
-            }) // Added clickable modifier here
+//            .clickable(onClick = {
+//                navController.navigate(Map.ofUser(person.name))
+//            }) // Added clickable modifier here
     ) {
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalAlignment = Alignment.Start
+                .padding(vertical = 10.dp, horizontal = 15.dp)
+//                verticalArrangement = Arrangement.spacedBy(8.dp),
+//                ,
+//                horizontalAlignment = Alignment.Start
+
+        ) {
+            InformationInPersonCard(person)
+        }
+
+        ElevatedButton(
+            onClick =
+            {
+                navController.navigate(Map.ofUser(person.name))
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = YellowPrimary,
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .padding(horizontal = 15.dp, vertical = 10.dp)
+                .fillMaxWidth()
         ) {
             Text(
-                text = "Name: ${person.name}",
-                style = MaterialTheme.typography.labelLarge,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-            Text(
-                text = "Location: ${person.location}",
-                style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-            Text(
-                text = "Temperature: ${person.temperature}",
-                style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-            Text(
-                text = "Phone Battery: ${person.phoneBattery}",
-                style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.onSurface
+                text = "Show Map",
+                color = Color.Black,
+                fontSize = 13.sp
             )
         }
+
+
+
     }
+}
+
+@Composable
+fun InformationInPersonCard(person:PersonInfo){
+
+    Text(
+        buildAnnotatedString(
+        ) {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                )
+            ) {
+                append("Name: ")
+            }
+            withStyle(
+                SpanStyle(
+                )
+            ) {
+                append(person.name)
+            }
+        }
+    )
+
+    Text(
+        buildAnnotatedString(
+        ) {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                )
+            ) {
+                append("Location: ")
+            }
+            withStyle(
+                SpanStyle(
+                )
+            ) {
+                append(person.location)
+            }
+        }
+    )
+
+    Text(
+        buildAnnotatedString(
+        ) {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                )
+            ) {
+                append("Temperature: ")
+            }
+            withStyle(
+                SpanStyle(
+                )
+            ) {
+                append(person.temperature)
+            }
+        }
+    )
+
+    Text(
+        buildAnnotatedString(
+        ) {
+            withStyle(
+                style = SpanStyle(
+                    fontWeight = FontWeight.Bold,
+                )
+            ) {
+                append("Phone Battery: ")
+            }
+            withStyle(
+                SpanStyle(
+                )
+            ) {
+                append(person.phoneBattery)
+            }
+        }
+    )
+//    Text(
+//        text = "Location: ${person.location}",
+//        style = MaterialTheme.typography.labelMedium,
+//        color = MaterialTheme.colorScheme.onSurface
+//    )
+//    Text(
+//        text = "Temperature: ${person.temperature}",
+//        style = MaterialTheme.typography.labelMedium,
+//        color = MaterialTheme.colorScheme.onSurface
+//    )
+//    Text(
+//        text = "Phone Battery: ${person.phoneBattery}",
+//        style = MaterialTheme.typography.labelMedium,
+//        color = MaterialTheme.colorScheme.onSurface
+//    )
 }
 
 // Example usage
